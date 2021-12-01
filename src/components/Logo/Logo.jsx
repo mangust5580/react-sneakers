d2d615360f2ed0f13ad051e9ef0918f417fa9994
mangust5580 <mangust5580@gmail.com>
2021-12-01 19:21:23 +0300

    начальная версия

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import styles from './Logo.module.scss';
 export const Logo = () => {
   return (
     <Link className={styles.logo} aria-label="Перейти на главную страницу" to="/">
-      <img src="./images/logo.png" alt="" width="345" height="41" />
+      <img src="images/logo.png" alt="" width="245" height="41" />
     </Link>
   );
 };
